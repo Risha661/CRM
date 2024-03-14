d44ed2c28f6179613b633488766ac3624d226e1a
unknown <git@github.com:Risha661/CRM.git>
2024-03-14 12:04:01 +0300

    Правки DOM_03

--- script.js
+++ script.js
@@ -26,80 +26,89 @@ const obj = {
 
 const goods = [
   {
-    "id": 1,
-    "title": "Смартфон Xiaomi 11T 8/128GB",
-    "price": 27000,
-    "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
-    "category": "mobile-phone",
-    "discont": false,
-    "count": 3,
-    "units": "шт",
-    "images": {
-      "small": "img/smrtxiaomi11t-m.jpg",
-      "big": "img/smrtxiaomi11t-b.jpg"
-    }
+    id: 34234353524553,
+    name: 'Смартфон Xiaomi 11T 8/128GB',
+    category: 'Смартфоны',
+    unit: 'шт',
+    quantity: 20,
+    price: '$500',
+    total: '$12000',
   },
   {
-    "id": 2,
-    "title": "Радиоуправляемый автомобиль Cheetan",
-    "price": 4000,
-    "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
-    "category": "toys",
-    "discont": 5,
-    "count": 1,
-    "units": "шт",
-    "images": {
-      "small": "img/cheetancar-m.jpg",
-      "big": "img/cheetancar-b.jpg"
-    }
+    id: 4242423434344,
+    name: 'Радиоуправляемый автомобиль Cheetan',
+    category: 'Игрушки',
+    unit: 'шт',
+    quantity: 1,
+    price: '$4000',
+    total: '$1000',
   },
-  {
-    "id": 3,
-    "title": "ТВ приставка MECOOL KI",
-    "price": 12400,
-    "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
-    "category": "tv-box",
-    "discont": 15,
-    "count": 4,
-    "units": "шт",
-    "images": {
-      "small": "img/tvboxmecool-m.jpg",
-      "big": "img/tvboxmecool-b.jpg"
-    }
-  },
-  {
-    "id": 4,
-    "title": "Витая пара PROConnect 01-0043-3-25",
-    "price": 22,
-    "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
-    "category": "cables",
-    "discont": false,
-    "count": 420,
-    "units": "v",
-    "images": {
-      "small": "img/lan_proconnect43-3-25.jpg",
-      "big": "img/lan_proconnect43-3-25-b.jpg"
-    }
-  }
 ];
 
 function createRow(obj) {
+  let index = 3;
+
   const row = document.createElement('tr');
+
+  const indexCell = document.createElement('td');
+  indexCell.classList.add('table__cell', 'table__cell_index');
+  row.appendChild(indexCell);
+
   for (const key in obj) {
     const cell = document.createElement('td');
-    cell.textContent = obj[key];
+    cell.classList.add('table__cell');
+    if (key === 'name') {
+      cell.classList.add('table__cell_left', 'table__cell_name');
+      const spanCell = document.createElement('span');
+      spanCell.classList.add('table__cell-id');
+      spanCell.textContent = `id: ${obj['id']}`;
+      cell.appendChild(spanCell);
+      cell.appendChild(document.createTextNode(obj[key]));
+    } else if (key === 'category') {
+      cell.classList.add('table__cell_left');
+      cell.textContent = obj[key];
+    } else {
+      cell.textContent = obj[key];
+    }
     row.appendChild(cell);
   }
-  return row;
+
+  const btnWrap = document.createElement('td');
+  btnWrap.classList.add('table__cell', 'table__cell_btn-wrapper');
+  row.appendChild(btnWrap);
+  const btnPic = document.createElement('button');
+  btnPic.classList.add('table__btn', 'table__btn_pic');
+  btnWrap.appendChild(btnPic);
+  const btnEdit = document.createElement('button');
+  btnEdit.classList.add('table__btn', 'table__btn_edit');
+  btnWrap.appendChild(btnEdit);
+  const btnDel = document.createElement('button');
+  btnDel.classList.add('table__btn', 'table__btn_del');
+  btnWrap.appendChild(btnDel);
+
+  return function() {
+    indexCell.textContent = index;
+    index++;
+    return row;
+  };
 };
 
 function renderGoods(goods) {
   const table = document.querySelector('.table__body');
-  goods.forEach(obj => {
-    row = createRow(obj);
+
+  let rowIndex = 2;
+  goods.forEach((obj) => {
+    const createRowWithIndex = createRow(obj);
+    const row = createRowWithIndex();
+    rowIndex++;
+    row.cells[0].textContent = rowIndex;
+    row.cells[1].remove();
     table.appendChild(row);
-});
-return table;
+    const rowCount = row.cells.length;
+    console.log('Количество строк в таблице:' + rowCount);
+  });
+
+  return table;
 };
 
 renderGoods(goods);
